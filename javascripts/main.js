55199b21826984bf60f57c157da37205457e7195
Thomas <thomas.vayne.alderson@gmail.com>
2017-12-06 14:45:44 +0000

    Changed index.html, main.js and main .css to add Issues Section to Website. Also added Project name based on review. (#30)

--- javascripts/main.js
+++ javascripts/main.js
@@ -186,3 +186,56 @@ function importSocialMediaWidgets() {
   script.setAttribute('async', true);
   document.head.appendChild(script);
 }
+
+//Import Latest Issues
+var issueOpenSvg =
+  '<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path fill-rule="evenodd" d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z" fill="white"></path></svg> ';
+var issueClosedSvg =
+  '<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="M7 10h2v2H7v-2zm2-6H7v5h2V4zm1.5 1.5l-1 1L12 9l4-4.5-1-1L12 7l-1.5-1.5zM8 13.7A5.71 5.71 0 0 1 2.3 8c0-3.14 2.56-5.7 5.7-5.7 1.83 0 3.45.88 4.5 2.2l.92-.92A6.947 6.947 0 0 0 8 1C4.14 1 1 4.14 1 8s3.14 7 7 7 7-3.14 7-7l-1.52 1.52c-.66 2.41-2.86 4.19-5.48 4.19v-.01z" fill="white"></path></svg> ';
+$.ajax({
+  type: "GET",
+  dataType: "json",
+  contentType: "application/json; charset=utf-8",
+  headers: { Accept: "application/vnd.github.v3+json" },
+  url:
+    "https://api.github.com/search/issues?q=@jboss-outreach+label:gci&sort=created&order=desc",
+  success: function(data, status, jqXHR) {
+    var result = $.parseJSON(JSON.stringify(data));
+    $("#issueList").fadeOut(function() {
+      $("#issueList").html(
+        '<div class="list-group-item row font-weight-bold"><strong><div class="col-xs-6">Issue Name</div><div class="col-xs-4">Repository</div><div class="col-xs-2"><span class="issueBadge">Status</span></div></strong></div>'
+      );
+      for (var i = 0; i < 10; i++) {
+        var item = result.items[i];
+        var myRegexp = /github\.com\/jboss-outreach\/(.+?)\/issues/i;
+        var match = myRegexp.exec(item.html_url);
+        var issueStateHTML =
+          '<span class="issueBadge ' +
+          (item.state == "open"
+            ? 'issueOpen">' + issueOpenSvg
+            : 'issueClosed">' + issueClosedSvg) +
+          item.state +
+          "</span>";
+        $("#issueList").append(
+          '<a href="' +
+            item.html_url +
+            '" class="row list-group-item ' +
+            (item.state == "open"
+              ? "list-group-item-success"
+              : "list-group-item-danger") +
+            '">' +
+            '<div class="col-xs-6">' +
+            item.title +
+            '</div><div class="col-xs-4">' +
+            match[1] +
+            '</div><div class="col-xs-2">' +
+            issueStateHTML +
+            "</div></a>"
+        );
+      }
+      $("#issueList").fadeIn();
+    });
+  }
+});
+
+
