2e2baac6b537834ed1f90dca028f94bb3922d4fb
Zhao Wei Liew <zhaoweiliew@gmail.com>
2017-12-23 21:19:09 +0800

    Remove unused code to get rid of console errors (#56)
    There is currently a bunch of unused code that spouts errors in the
    console due to missing dependencies. Remove all that unused code
    so that the console remains clean.
    
    Stuff removed include:
     * Loklak scripts
     * Google maps scripts
     * Image slider scripts
     * Social widget scripts

--- javascripts/main.js
+++ javascripts/main.js
@@ -176,20 +176,6 @@ $(".close").click(function() {
     // window.opener.location.reload(false);
 });
 
-// Import social media widgets
-if (document.readyState === "complete") {
-  importSocialMediaWidgets();
-} else {
-  window.addEventListener('load', importSocialMediaWidgets);
-}
-
-function importSocialMediaWidgets() {
-  var script = document.createElement('script');
-  script.setAttribute('src', '/javascripts/social-widgets-loader.js');
-  script.setAttribute('async', true);
-  document.head.appendChild(script);
-}
-
 //Import Latest Issues
 var issueOpenSvg =
   '<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path fill-rule="evenodd" d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z" fill="white"></path></svg> ';
