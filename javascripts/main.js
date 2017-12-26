f7be1e476ae2af5ae21c51e6e068f0f2f5373733
Sachin Rammoorthy <sachin.rammoorthy@gmail.com>
2017-12-27 01:47:07 +0530

    Added slider

--- javascripts/main.js
+++ javascripts/main.js
@@ -64,6 +64,57 @@ $(function() {
     }
   });
 });
+
+
+
+
+var slideIndex = 0;
+showSlides(slideIndex);
+carousel();
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}    
+    x[slideIndex-1].style.display = "block";  
+    setTimeout(carousel, 4000); // Change image every 2 seconds
+    dots[slideIndex-1].className += " active";
+}
+
+
+
 // Start ignoring JSHintBear
 $(function() {
   var issueElement;
