fcf16551468dfb87aacf2b950e411cda0d302ccc
Divyansh Kulshreshtha <k.divyansh268@gmail.com>
2018-01-17 12:33:34 +0530

    Upgrade to Font awesome v5 + code cleanup (#114)
    * Upgrade to Font awesome v5 + code cleanup
    
    * Minor styling changes + css alignment
    
    * trigger another build for bot

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,7 +4,7 @@ window.onload = function() {
   }).done(function(data) {
     data.forEach(function(repo) {
       var divRepeated = document.getElementById("repeatedCode");
-      var code = "<div class='col-xs-12 col-sm-6 col-md-4 col-lg-3'><div class='card'><div class='avatar'><img id='projectImage" + repo.name + "' src='images/projects/project.png' alt='Project of JBoss'><div class='social' style='font-size: 20px; margin-top: -35px;'><b><p id='projectHeading" + repo.name + "' style='font-size: 25px;'> </p>contributors</b><br><a target='_blank' href='//github.com/jboss-outreach/" + repo.name + "'><i class='fa fa-github' style='font-size: 30px; margin: 5px;''></i></a></div><p id='projectTitle" + repo.name + "' style='display: inline-block;'></p></div></div></div>";
+      var code = "<div class='col-xs-12 col-sm-6 col-md-4 col-lg-3'><div class='card'><div class='avatar'><img id='projectImage" + repo.name + "' src='images/projects/project.png' alt='Project of JBoss'><div class='social' style='font-size: 20px; margin-top: -35px;'><b><p id='projectHeading" + repo.name + "' style='font-size: 25px;'> </p>contributors</b><br><a target='_blank' href='//github.com/jboss-outreach/" + repo.name + "'><i class='fab fa-github' style='font-size: 30px; margin: 5px;''></i></a></div><p id='projectTitle" + repo.name + "' style='display: inline-block;'></p></div></div></div>";
       divRepeated.insertAdjacentHTML('afterbegin', code);
       projectContributors(repo.name, repo.description);
     });
@@ -68,12 +68,12 @@ async function loadContributors() {
     .forEach((contributor) => {
       let html = "<div class='col-xs-12 col-sm-6 col-md-4 col-lg-3'><div class='card'><div class='avatar'>";
       html += "<img src=" + contributor.avatar_url + "><div class='contribs'><p style='margin-top: -15.5px !important;'>";
-      html += contributor.contributions + " <br>contribution";
+      html += contributor.contributions + " <br>Contribution";
       if (contributor.contributions > 1) {
         html += "s";
       }
       html += "</p><a href=" + contributor.html_url +
-        " class='contributor-gh'><i class='fa fa-github fa-2x' aria-hidden='true'></i></a></div>";
+        " class='contributor-gh'><i class='fab fa-github fa-2x' aria-hidden='true'></i></a></div>";
       html += "<span>" + contributor.login + "</span></div></div></div>";
       $("#contributors-list").append(html);
     });
@@ -142,80 +142,6 @@ function carousel() {
     dots[slideIndex-1].className += " active";
 }
 
-
-
-// Start ignoring JSHintBear
-$(function() {
-  var issueElement;
-  $.ajax({
-    url: "https://api.github.com/repos/fossasia/gci16.fossasia.org/issues?state=open"
-  }).done(function(data) {
-    var labels = [];
-    var labelNames = [];
-    var mainWrapper = $("#issues-categories-wrapper");
-    for (var i = 0; i < data.length; i++) {
-      if (data[i].hasOwnProperty("pull_request") || data[i].closed_at !== null) {
-        data.splice(i, 1);
-        i--;
-        continue;
-      }
-    }
-    for (i = 0; i < data.length; i++) {
-      if (data[i].labels.length === 0) {
-        issueElement = $('<div class="issue"></div>')
-                .append($('<div class="issue-left"></div>')
-                  .append($("<span></span>").append(data[i].number))
-                  .append($("<a></a>").attr("target", "_blank").attr("href", data[i].html_url).append(data[i].title))
-                  .append($("<p>Opened by </p>").append($("<a></a>").append(data[i].user.login).attr("href", data[i].user.html_url).attr('target', '_blank')))
-                )
-                .append($('<div class="issue-right"></div>')
-                  .append($("<a class='comments'></a>")
-                    .attr("href", data[i].html_url)
-                    .attr('target', '_blank')
-                    .append($("<i class='fa fa-comment'></i>"))
-                    .append(data[i].comments))
-                );
-        issueElement.appendTo($("#unlabeled-category"));
-      }
-      for (var j = 0; j < data[i].labels.length; j++) {
-        if ($.inArray(data[i].labels[j].name, labelNames) === -1) {
-          labelNames.push(data[i].labels[j].name);
-          labels.push(data[i].labels[j]);
-        }
-      }
-    }
-    for (i = 0; i < labels.length; i++) {
-      var categoryElement = $('<div class="issues-category"></div>');
-      var titleButton = $('<div class="button"></div>')
-          .append($('<a></a>').append(labels[i].name).attr("href", labels[i].html_url))
-          .css("background", "#" + labels[i].color);
-      categoryElement.append(titleButton);
-      for (j = data.length - 1; j >= 0; j--) {
-        for (var k = data[j].labels.length - 1; k >= 0; k--) {
-          if (data[j].labels[k].name === labels[i].name) {
-            // all hail .append()
-            // build the issue element
-            issueElement = $('<div class="issue"></div>')
-                .append($('<div class="issue-left"></div>')
-                  .append($("<span></span>").append(data[j].number))
-                  .append($("<a></a>").attr("target", "_blank").attr('href', data[j].html_url).append(data[j].title))
-                  .append($("<p>Opened by </p>").append($("<a></a>").append(data[j].user.login).attr("href", data[j].user.html_url).attr('target', '_blank')))
-                )
-                .append($('<div class="issue-right"></div>')
-                  .append($("<a class='comments'></a>")
-                    .attr("href", data[j].html_url)
-                    .attr('target', '_blank')
-                    .append($("<i class='fa fa-comment'></i>"))
-                    .append(data[j].comments))
-                );
-            categoryElement.append(issueElement);
-          }
-        }
-      }
-      mainWrapper.append(categoryElement);
-    }
-  });
-});
 $(document).ready(function() {
   $(document).on("scroll", onScroll);
 });
