6a614b3962608a5701a0de9b58ffe57c06d6c5ea
code_damn_it <sabaxuxunashvili@gmail.com>
2018-09-12 12:31:43 +0400

    Issue #115 | Fixed GCI Logo drop by changing ncb (Navbar collapse breakpoint) & M=more... (#127)
    * navbar logo fix
    
    * Remove unused code, fixed broken links & more
    
    * Refactored slideshow code

--- javascripts/main.js
+++ javascripts/main.js
@@ -28,7 +28,7 @@ function projectContributors(name, description){
             }
 
         });
-} 
+}
 
 let allContributors = [];
 
@@ -94,53 +94,6 @@ $(function() {
     }
   });
 });
-
-
-
-var slideIndex = 0;
-showSlides(slideIndex);
-carousel();
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1}    
-    x[slideIndex-1].style.display = "block";  
-    setTimeout(carousel, 5000);
-    dots[slideIndex-1].className += " active";
-}
 
 $(document).ready(function() {
   $(document).on("scroll", onScroll);
@@ -234,5 +187,3 @@ $.ajax({
     });
   }
 });
-
-
---
+++ javascripts/slideshow.js
@@ -0,0 +1,74 @@
+var slides = [];
+
+slides.push({
+  img: "http://www.wethetalent.co/wp-content/uploads/2017/08/1400_700-QSfR8wHNRjrYs5hfY6QKKLviQvrlXldmrtC85OoJslZcPOzXcTtVSTLZ5cW5QQZI.jpg",
+  headerText: "BUILD. LEARN. WIN.",
+  lowerHTML: "if($('#you') == 'code-lover') <br> print('Come hack with us!');",
+});
+slides.push({
+  img: "https://www.veracode.com/sites/default/files/shutterstock_601201364%20%281%29.jpg",
+  headerText: "JOIN US!",
+  lowerHTML: "The JBoss Community<br><br><a href='https://codein.withgoogle.com/archive/2017/organization/5717119551406080/'><button class='buttonStyle buttonRed'>Check us out!</button></a>",
+});
+slides.push({
+  img: "https://www.veracode.com/sites/default/files/shutterstock_133524401.jpg",
+  headerText: "INTRIGUING BLOGS",
+  lowerHTML: "Read our thought-provoking stories<br><br><a href='https://blog.jboss-outreach.org/'><button class='buttonStyle buttonRed'>Read</button></a>'",
+});
+
+
+// add 'dots' of slideshow
+var dots = "";
+for(var i=0; i<slides.length; i++){
+  dots += "<span class='dot' onclick='showSlides("+i+")'></span>";
+}
+$("#dots-wrapper").html(dots);
+
+
+var slideShowEngine;
+var slideIndex = 0;
+
+
+
+function carousel() {
+  window.clearTimeout(slideShowEngine);
+  showSlides(slideIndex);
+  slideIndex+=1;
+  if(slideIndex === slides.length){
+    slideIndex = 0;
+  }
+  slideShowEngine = setTimeout(carousel, 5000);
+}
+carousel();
+
+
+function plusSlides(n) {
+  slideIndex+=n;
+  if(slideIndex<0){
+    slideIndex = slides.length-1;
+  }
+  if(n<0 && slideIndex===slides.length-1){
+    slideIndex = slides.length-2;
+  }
+  carousel();
+}
+
+function showSlides(n) {
+  if(n<0 || n>slides.length){
+    slideIndex = 0;
+    break;
+  }
+  // Replace data
+  $("#slideImage").attr("src", slides[n].img);
+  $("#slideTitle").text(slides[n].headerText);
+  $("#slideLower").html(slides[n].lowerHTML);
+
+  // Change current slide's dots class to active
+  $(".dot").each(function(i, obj){
+    if(i === n){
+      $(obj).addClass("active");
+    }else{
+      $(obj).removeClass("active");
+    }
+  });
+}
